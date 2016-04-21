aaf8b5c98779a5f38e6d4092937ba021f9e6dc2b
Aaron Erlich <aaron.erlich@gmail.com>
2016-04-21 14:47:08 -0700

    Revert "attempt to flatten list of traces passed to tile and check if they are of the rigth class simultaneously"
    This reverts commit b50e881f3fde7c091cdc533167297fa27d653b1b.

--- tile/R/tileProcessTraces.R
+++ tile/R/tileProcessTraces.R
@@ -1,61 +1,42 @@
-#there is no easy way to flatten such a list that is vectorizable
-#so do checking of tileTrace type here
-flatten1 <- function(traces) {
-  newtraces <- list()
-  for (i in 1:length(traces)) {
-    if (any(class(traces[[i]]) == "tileTrace")) {
-      newtraces[[length(newtraces) + 1]] <- traces[[i]]
-    } else if (any(class(traces[[i]]) == "list")) {
-        for (j in 1:length(traces[[i]])) {
-          newtraces[[length(newtraces) + 1]] <- traces[[i]][[j]]
-        }
-      } else {
-          stop("You have provided an object that is not a tile trace")
-        }
-  }
-  print(length(newtraces[[3]]))
-  return(newtraces)
-}
-
 tileProcessTraces <- function(traces) {
 
-    # Unbundle and check for mimimal requirements
-    newtraces <- flatten1(traces)  
+    # Check for mimimal requirements
+
     # Make new list
 
-#     # Unbundle any traces bundled into lists
-#     ntraces <- length(traces)
-#     nnewtraces <- 0
-#     for (i in 1:ntraces) {
-#       if (any(class(traces[[i]])=="tileTrace")) {
-#         nnewtraces <- nnewtraces + 1
-#       } else {
-#         if (is.list(traces[[i]])) {
-#           for (j in 1:length(traces[[i]])) {
-#             if (any(class(traces[[i]])=="tileTrace")) {
-#               nnewtraces <- nnewtraces + 1              
-#             }            
-#           }
-#         }
-#       }
-#     }
-#     newtraces <- vector("list",nnewtraces)
-#     int <- 1
-#     for (i in 1:ntraces) {
-#       if (any(class(traces[[i]])=="tileTrace")) {
-#         newtraces[[int]] <- traces[[i]]
-#         int <- int + 1
-#       } else {
-#         if (is.list(traces[[i]])) {
-#           for (j in 1:length(traces[[i]])) {
-#             if (any(class(traces[[i]][[j]])=="tileTrace")) {
-#               newtraces[[int]] <- traces[[i]][[j]]
-#               int <- int + 1          
-#             }            
-#           }
-#         }
-#       }
-#     }
+    # Unbundle any traces bundled into lists
+    ntraces <- length(traces)
+    nnewtraces <- 0
+    for (i in 1:ntraces) {
+      if (any(class(traces[[i]])=="tileTrace")) {
+        nnewtraces <- nnewtraces + 1
+      } else {
+        if (is.list(traces[[i]])) {
+          for (j in 1:length(traces[[i]])) {
+            if (any(class(traces[[i]])=="tileTrace")) {
+              nnewtraces <- nnewtraces + 1              
+            }            
+          }
+        }
+      }
+    }
+    newtraces <- vector("list",nnewtraces)
+    int <- 1
+    for (i in 1:ntraces) {
+      if (any(class(traces[[i]])=="tileTrace")) {
+        newtraces[[int]] <- traces[[i]]
+        int <- int + 1
+      } else {
+        if (is.list(traces[[i]])) {
+          for (j in 1:length(traces[[i]])) {
+            if (any(class(traces[[i]][[j]])=="tileTrace")) {
+              newtraces[[int]] <- traces[[i]][[j]]
+              int <- int + 1          
+            }            
+          }
+        }
+      }
+    }
     ntraces <- length(newtraces)
     
     # Loop over all traces; fill in defaults; determine number of trace-plots    
@@ -66,9 +47,7 @@ tileProcessTraces <- function(traces) {
         tracedefault <- eval(call(paste(newtraces[[i]]$graphic,"TileTraceDefaults",sep="")))
 
         # Fillout trace options
-        newtraces[[i]] <- eval(call(paste(newtraces[[i]]$graphic,
-                                          "TileTraceFillout",sep=""),
-                                    newtraces[[i]],tracedefault))
+        newtraces[[i]] <- eval(call(paste(newtraces[[i]]$graphic,"TileTraceFillout",sep=""),newtraces[[i]],tracedefault))
 
         if (is.null(newtraces[[i]]$plot))
             newtraces[[i]]$plot <- i
@@ -82,9 +61,7 @@ tileProcessTraces <- function(traces) {
     for (i in 1:ntraces) {
 
         # Optional special treatment based on graphic type
-        newtraces[[i]] <- eval(call(paste(newtraces[[i]]$graphic,
-                                          "TilePrep",sep=""),
-                                    newtraces[[i]]))
+        newtraces[[i]] <- eval(call(paste(newtraces[[i]]$graphic,"TilePrep",sep=""),newtraces[[i]]))
         
         # Create one copy of the trace for each plot it appears in
         currplots <- newtraces[[i]]$plot
@@ -94,5 +71,6 @@ tileProcessTraces <- function(traces) {
             tracesout[[ip]]$plot <- currplots[k]
         }
     }
-    return(tracesout)
+    
+    tracesout
 }
